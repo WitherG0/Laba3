eeb9f3c0b2b8efa29464cca578d8ca5951390e1f
WitherG0 <91891123+WitherG0@users.noreply.github.com>
2021-12-05 20:46:43 +0300

    Update Program.cs

--- 5/Program.cs
+++ 5/Program.cs
@@ -1,45 +1,64 @@
 ﻿using System;
 
-namespace Fifth
+namespace ConsoleApplication1
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             Random rand = new Random();
-            int[,] matrix1 = new int[5, 5];
+            int[,] A = new int[5, 5];
             for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine();
                 for (int j = 0; j < 5; j++)
                 {
-                    matrix1[i, j] = rand.Next(1, 10);
-                    Console.Write(matrix1[i, j] + " ");
+                    A[i, j] = rand.Next(1, 10);
                 }
             }
-            Console.WriteLine();
-            int[,] matrix2 = new int[5, 5];
+           
+            int[,] B = new int[5, 5];
             for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine();
                 for (int j = 0; j < 5; j++)
                 {
-                    matrix2[i, j] = rand.Next(1, 10);
-                    Console.Write(matrix2[i, j] + " ");
+                    B[i, j] = rand.Next(1, 10);
                 }
             }
-            Console.WriteLine();
-            int[,] ansver = new int[5, 5];
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine();
-                for (int j = 0; j < 5; j++)
-                {
-                    ansver[i, j] = matrix1[i, j] * matrix2[i, j];
-                    Console.Write(ansver[i, j] + " ");
-                }
-            }
+            
+            Console.WriteLine("\nМатрица A:");
+            Print(A);
+            Console.WriteLine("\nМатрица B:");
+            Print(B);
+            Console.WriteLine("\nМатрица C = A * B:");
+            int[,] C = Multiplication(A, B);
+            Print(C);
 
+        }
+        static int[,] Multiplication(int[,] a, int[,] b)
+        {
+            int[,] r = new int[5,5];
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    for (int k = 0; k < b.GetLength(0); k++)
+                    {
+                        r[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+            return r;
+        }
+        static void Print(int[,] a)
+        {
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", a[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
